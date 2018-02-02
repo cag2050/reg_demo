ad491ff9ed2e5d643871bc84e7faf9f3b6ad968a
cag2050 <cag2050@163.com>
2018-02-02 17:42:02 +0800

    m

--- str.match(reg).js
+++ str.match(reg).js
@@ -1,6 +1,7 @@
+
+// 存放匹配结果的数组。该数组的内容依赖于 regexp 是否具有全局标志 g。 如果没找到匹配结果返回 null 。
+
 let str = 'a101';
-let matchArr = str.match(/[0-9]/g)
-let matchStr = matchArr.join('')
+let matchArr = str.match(/[1]/g)
 
 console.log(matchArr)
-console.log(matchStr)
