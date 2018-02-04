97d4e338fae66f99f380f82bcb0c3d055f17b5d9
cag2050 <cag2050@163.com>
2018-02-04 21:02:09 +0800

    m

--- str.search(reg|str).js
+++ str.search(reg|str).js
@@ -1,12 +1,13 @@
 
 // 如果匹配成功，则 search() 返回正则表达式在字符串中首次匹配项的索引。否则，返回 -1。
 
-let str = '1111abc'
-let searchRegReturn = str.search(/x/)
+let str = '1111abcabc'
+let searchRegReturn = str.search(/x/g)
+console.log('返回值:stringObject 中第一个与 regexp 相匹配的子串的起始位置。' +
+    '注释：如果没有找到任何匹配的子串，则返回 -1。')
 console.log(searchRegReturn)
 
-let str2 = '1111abc'
-let searchStrReturn = str2.search('bc')
+let searchStrReturn = str.search('bc')
 console.log(searchStrReturn)
 
 
